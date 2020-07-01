4e9340b961b9096f7d8eeeff98584022429ee2de
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-01 17:16:16 -0400

    Added second_button Button and editText_second EditText to SecondActivity. Added returnReply method to SecondActivity

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.twoactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +18,8 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReply(View view) {
     }
 }
