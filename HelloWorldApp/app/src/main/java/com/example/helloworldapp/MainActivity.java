fde4fafc89dfdb2a0a5bc10fed6987255da39d2d
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-01 12:37:33 -0400

    Added Log.d call to onCreate

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.helloworldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Hello World");
     }
 }
