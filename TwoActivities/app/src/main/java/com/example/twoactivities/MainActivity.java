a0b6698accc8f37aeae191026e438fe94769d438
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-01 17:01:27 -0400

    Added and instantiated mMessageEditText variable in MainActivity

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -4,18 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "MainActivity";
+    private static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extras.MESSAGE";
+
+    private EditText mMessageEditText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
