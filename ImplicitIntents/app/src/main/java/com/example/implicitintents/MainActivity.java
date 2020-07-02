ca1a3d8f4218cc0d2b623e4d21592830937f6e2a
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-02 10:28:49 -0400

    Added and instantiated member variables in MainActivity

--- ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocEditText;
+    private EditText mShareEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocEditText = findViewById(R.id.loc_edittext);
+        mShareEditText = findViewById(R.id.share_edittext);
+
     }
 
     public void openWebsite(View view) {
