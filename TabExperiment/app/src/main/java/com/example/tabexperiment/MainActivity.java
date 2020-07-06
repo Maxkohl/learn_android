90f705a28c007798eb4720fa756bee76f6617eba
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-06 16:05:56 -0400

    Instantiated and inflated toolbar and tablayout

--- TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
+++ TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 }
