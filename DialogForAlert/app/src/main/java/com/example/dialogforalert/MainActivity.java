85bdbd7c8e9a045bf939b9e7309cfa7fedad82a8
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-06 12:56:07 -0400

    Created a Dialog Builder. Added title, message, and onClick listeners for OK and CANCEL options

--- DialogForAlert/app/src/main/java/com/example/dialogforalert/MainActivity.java
+++ DialogForAlert/app/src/main/java/com/example/dialogforalert/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dialogforalert;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickShowAlert(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.alert_title).setMessage(R.string.alert_message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You clicked CANCEL", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+
     }
+
+    
 }
