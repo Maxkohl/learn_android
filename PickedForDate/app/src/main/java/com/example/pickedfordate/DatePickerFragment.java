71c9ccf0bd0e405ead904d662cdc5b950e8f0678
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-06 14:10:04 -0400

    Created onCreateDialog and instantiated year, month, date

--- PickedForDate/app/src/main/java/com/example/pickedfordate/DatePickerFragment.java
+++ PickedForDate/app/src/main/java/com/example/pickedfordate/DatePickerFragment.java
@@ -1,33 +1,45 @@
 package com.example.pickedfordate;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DatePickerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class DatePickerFragment extends Fragment {
+import java.util.Calendar;
+
+public class DatePickerFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-           
+
         }
     }
 
+    @NonNull
+    public Dialog onCreateDialg(Bundle savedInstanceBundle) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date_picker, container, false);
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+
     }
 }
