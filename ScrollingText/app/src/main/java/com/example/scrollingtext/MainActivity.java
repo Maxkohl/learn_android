14812d54f38ced1707f2a015ee00dd5ea74ba81f
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-06 12:42:07 -0400

    Created context menu. Added strings to resource. Added to MainActivity

--- ScrollingText/app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ ScrollingText/app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.scrollingtext;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView article_text = findViewById(R.id.article);
+        registerForContextMenu(article_text);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_edit):
+                displayToast("You chose edit!");
+                break;
+            case (R.id.action_share):
+                displayToast("You chose share!");
+                break;
+            case (R.id.action_delete):
+                displayToast("You chose delete!");
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
