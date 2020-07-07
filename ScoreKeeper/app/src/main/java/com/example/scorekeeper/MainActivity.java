2913aa8e6d4bd6fc59a89dbadb154c64fb5efe20
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-07 12:25:11 -0400

    Added onClick handlers to buttons and created decreaseScore and increaseScore methods in MainActivity

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int score1;
+    private int score2;
+    private TextView mScore1Text;
+    private TextView mScore2Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScore1Text = (TextView)findViewById(R.id.score_team1);
+        mScore2Text = (TextView)findViewById(R.id.score_team2);
+
+    }
+
+    public void decreaseScore(View view) {
+    }
+
+    public void increaseScore(View view) {
     }
 }
