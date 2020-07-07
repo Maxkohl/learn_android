88906b38c061f2980e4f70259686a7f29281dcc1
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-07 10:48:55 -0400

    Implemented onCreateViewHolder and onBindViewHolder

--- RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.recyclerview;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +16,27 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     private LinkedList<String> mWordList;
     private LayoutInflater mInflater;
 
-    @NonNull
-    @Override
-    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public WordListAdapter(LinkedList<String> mWordList, Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = mWordList;
     }
 
+    // The onCreateViewHolder() method is similar to the onCreate() method. Inflates the item
+    // layout, and returns a ViewHolder with the layout and the adapter.
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                             int viewType) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+
+    //method connects your data to the view holder.
     @Override
     public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
@@ -36,7 +49,7 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
         final WordListAdapter mAdapter;
 
 
-        public WordViewHolder(@NonNull View itemView, TextView wordItemView, WordListAdapter mAdapter) {
+        public WordViewHolder(@NonNull View itemView, WordListAdapter mAdapter) {
             super(itemView);
             this.wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = mAdapter;
