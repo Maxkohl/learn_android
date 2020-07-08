4a474833eba58a84e07f193a6a44fe7be8365906
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-08 17:53:15 -0400

    Created createNotificationChannel method and built notificationChannel in it\

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
 
     private Button button_notify;
 
@@ -23,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void createNotificationChannel() {
+        //Create Notification Manager
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            //Create notification channel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 
     private void sendNotification() {
