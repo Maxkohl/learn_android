22dbf96b6c54b41c03f34813ac11712ba3b21c10
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-08 15:29:41 -0400

    Created CustomReceiver for broadcasts. Instantiated in MainActivity and added Intent Filter actions in onCreate

--- PowerReceiver/app/src/main/java/com/example/powerreceiver/MainActivity.java
+++ PowerReceiver/app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.powerreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Create Custom dynamic receiver
+    private CustomReceiver mReceiver = new CustomReceiver();
+    //Create intent filter with intent actions
+    IntentFilter filter = new IntentFilter();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add Intent actions to the intent filter
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        
     }
 }
