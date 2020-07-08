20b0bfd0a6fb420dca42d1dcca2b9c9dc1a206f9
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-08 10:26:57 -0400

    Created and passed launchActivity test

--- TwoActivities/app/src/androidTest/java/com/example/twoactivities/ActivityInputOutputTest.java
+++ TwoActivities/app/src/androidTest/java/com/example/twoactivities/ActivityInputOutputTest.java
@@ -10,6 +10,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -28,5 +33,13 @@ public class ActivityInputOutputTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.twoactivities", appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLauncher() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_second)).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
     }
 }
