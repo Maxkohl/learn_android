5fd4620c63e1aad2a83e7b142024604e27061e20
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-08 17:38:48 -0400

    Added Notify Me button and created sendNotification method in MainActivity

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.notifyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+
+    }
+
+    private void sendNotification() {
+        
     }
 }
