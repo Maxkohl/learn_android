cf276f19e086a49f2cf9f3901b29ae6bc6e95f64
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-08 10:32:03 -0400

    Created and passed textInputOutput test

--- TwoActivities/app/src/androidTest/java/com/example/twoactivities/ActivityInputOutputTest.java
+++ TwoActivities/app/src/androidTest/java/com/example/twoactivities/ActivityInputOutputTest.java
@@ -12,9 +12,11 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -41,5 +43,13 @@ public class ActivityInputOutputTest {
         onView(withId(R.id.text_header)).check(matches(isDisplayed()));
         onView(withId(R.id.button_second)).perform(click());
         onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.editText_main)).perform(typeText("Test Message"));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("Test Message")));
+
     }
 }
