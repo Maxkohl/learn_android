e6fd7b74ed19ad90dcd922fd9b75d9e8d66aa43c
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-09 15:36:35 -0400

    Built notification in onStartJob

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -2,8 +2,10 @@ package com.example.notificationscheduler;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 
@@ -19,6 +21,13 @@ public class NotificationJobService extends JobService {
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         createNotificationChannel();
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+                PRIMARY_CHANNEL_ID).setContentTitle(getString(R.string.job_notification_title)).setContentText(getString(R.string.job_notification_text)).setContentIntent(contentPendingIntent).setSmallIcon(R.drawable.ic_job_running).setPriority(NotificationCompat.PRIORITY_HIGH).setDefaults(NotificationCompat.DEFAULT_ALL).setAutoCancel(true);
+        mNotificationManager.notify(0, builder.build());
+
         return false;
 
     }
