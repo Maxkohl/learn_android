9289f920f2f3a711b6ed7a6fa4cf25697fa998c5
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-09 15:13:06 -0400

    Created NotificationJobService class

---
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,20 @@
+package com.example.notificationscheduler;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class NotificationJobService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
