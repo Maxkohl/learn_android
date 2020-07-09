6291d063bcf3c9b7e81cba7059ef139521f444fc
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-09 16:00:51 -0400

    Built JobInfo in scheduleJob and displayed Toast informing user job is scheduled

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.java
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.notificationscheduler;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private JobScheduler mScheduler;
+    private static final int JOB_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void scheduleJob(View view) {
         RadioGroup networkOptions = findViewById(R.id.networkOptions);
         int selectedNetworkId = networkOptions.getCheckedRadioButtonId();
         int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
 
         switch (selectedNetworkId) {
             case R.id.noNetwork:
                 selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
-                        break;
+                break;
             case R.id.anyNetwork:
                 selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
                 break;
@@ -32,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
                 break;
         }
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder.setRequiredNetworkType(selectedNetworkOption);
+
+        JobInfo jobInfo = builder.build();
+        mScheduler.schedule(jobInfo);
+        Toast.makeText(this, getString(R.string.job_scheduled_notice), Toast.LENGTH_SHORT).show();
 
     }
 }
