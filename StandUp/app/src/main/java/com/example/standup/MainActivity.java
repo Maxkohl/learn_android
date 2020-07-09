49b55b30688ae2447c1ca26bdbf8704330fe1930
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-09 12:04:21 -0400

    Instantiated alarmToggle in MainActivity and set listener to display toast message when alarm is enabled or disabled

--- StandUp/app/src/main/java/com/example/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.standup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                String toastMessage;
+                if (isChecked) {
+                    toastMessage = getString(R.string.alarm_set_toast);
+                } else {
+                    toastMessage = getString(R.string.alarm_disabled_toast);
+                }
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
