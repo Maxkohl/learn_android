b512d4417472daf20e57adabf98aa126eaa47d9a
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-09 15:23:38 -0400

    Made running icon. Created createNotificationChannel method in NotificationJobService

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -1,20 +1,46 @@
 package com.example.notificationscheduler;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.graphics.Color;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class NotificationJobService extends JobService {
+    NotificationManager mNotificationManager;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
+        createNotificationChannel();
         return false;
+
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
         return false;
+    }
+
+    public void createNotificationChannel() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(PRIMARY_CHANNEL_ID, "Job Service Notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Job Service");
+
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
