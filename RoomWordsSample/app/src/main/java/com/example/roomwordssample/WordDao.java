9122f7e5006507841f9f2efaf1dca63e63fff8a0
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-10 17:43:01 -0400

    Wrapped list returned by getAllWords in LiveData wrapper

--- RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordDao.java
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -1,5 +1,6 @@
 package com.example.roomwordssample;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -14,5 +15,5 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 }
