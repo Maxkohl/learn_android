9cd1a550825d048a53975328696e6d0367067f8d
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-10 17:13:51 -0400

    Created Word class and set constructor and getter

---
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/Word.java
@@ -0,0 +1,12 @@
+package com.example.roomwordssample;
+
+import androidx.annotation.NonNull;
+
+public class Word {
+    private String mWord;
+    public Word(@NonNull String word){this.mWord = word;}
+
+    public String getmWord() {
+        return mWord;
+    }
+}
