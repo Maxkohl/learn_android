340ed07ea76685a7a5cc3f973b499a544f2e5486
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-10 14:19:36 -0400

    Created SettingFragment shell

---
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingFragment.java
@@ -0,0 +1,25 @@
+package com.example.appwithsettings;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+}
