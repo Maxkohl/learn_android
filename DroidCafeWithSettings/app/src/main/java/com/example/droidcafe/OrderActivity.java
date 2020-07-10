d5f4538683e45f2afd55804b1946afedebc945a0
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-10 14:30:22 -0400

    Initial commit of DroidCafeWithSettings

---
+++ DroidCafeWithSettings/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -0,0 +1,82 @@
+package com.example.droidcafe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String spinnerLabel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        TextView orderMessage = findViewById(R.id.order_message);
+        orderMessage.setText(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.label_array, android.R.layout.simple_spinner_dropdown_item);
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.same_day:
+                if (checked) {
+                    displayToast(getString(R.string.same_day_message));
+                }
+                break;
+            case R.id.next_day:
+                if (checked) {
+                    displayToast(getString(R.string.next_day_message));
+                }
+                break;
+            case R.id.pickup:
+                if (checked) {
+                    displayToast(getString(R.string.self_pickup_message));
+                }
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+}
