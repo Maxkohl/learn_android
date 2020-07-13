0793dcc77324895cf3dd084f6649270683cf8d8e
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 11:34:10 -0400

    Added RoomDatabase Callback and PopulateDbAsync to WordRoomDatabase

--- RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.example.roomwordssample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -21,12 +24,40 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     //create Database with database builder
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            WordRoomDatabase.class, "word_database").fallbackToDestructiveMigration().build();
+                            WordRoomDatabase.class, "word_database").fallbackToDestructiveMigration().addCallback(sRoomDatabaseCallback).build();
                 }
             }
         }
         return INSTANCE;
     }
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
 
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao wordDao;
+        String[] words = {"Cat", "Dog", "Chicken"};
+
+        private PopulateDbAsync(WordRoomDatabase db) {
+            this.wordDao = db.wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            wordDao.deleteAll();
+            for (int i = 0; i < words.length; i++) {
+                Word word = new Word(words[i]);
+                wordDao.insert(word);
+            }
+            return null;
+        }
+    }
 }
