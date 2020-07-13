efffb33d1e446df9185f202261000e1b6ce0a940
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 12:24:36 -0400

    Added onClick listener to save button

--- RoomWordsSample/app/src/main/java/com/example/roomwordssample/NewWordActivity.java
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/NewWordActivity.java
@@ -2,13 +2,41 @@ package com.example.roomwordssample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewWordActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.roomwordssample.REPLY";
+    private EditText mEditWordView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
+        mEditWordView = findViewById(R.id.edit_word);
+
+        final Button saveButton = findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                if (TextUtils.isEmpty(mEditWordView.getText())) {
+                    setResult(RESULT_CANCELED, intent);
+                } else {
+                    String word = mEditWordView.getText().toString();
+                    intent.putExtra(EXTRA_REPLY, word);
+                    setResult(RESULT_OK, intent);
+                }
+                finish();
+            }
+        });
     }
+
+
 }
