9cd3c6c6261aaf8daf5cc9c4a873b8c407f4e1e6
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 10:39:00 -0400

    Instantiated WordDao and WordRoomDatabase. Made WordRoomDatabase a singleton as to not open multiple instances

--- RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -1,7 +1,32 @@
 package com.example.roomwordssample;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
-    
+
+    public WordDao wordDao;
+
+    private static WordRoomDatabase INSTANCE;
+
+    //Method makes the WordRoomDatabase a singleton. Makes it so you can't have multiple
+    // instances of room database.
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    //create Database with database builder
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database").fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
 }
